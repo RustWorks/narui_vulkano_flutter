c1af18567e8b3e4a58e23875ff7363133490f908
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2021-08-26 00:13:16 +0200

    optimize keys

--- examples/circle_bench.rs
+++ examples/circle_bench.rs
@@ -1,14 +1,7 @@
 use narui::*;
+use narui_widgets::*;
 use winit::{platform::unix::WindowBuilderExtUnix, window::WindowBuilder};
-/*
-                            <fill_rect
-                                key=&y
-                                fill_color={
-                                    let val = context.listen(frame_counter);
-                                    Some(Color::from_components((x as f32 / 50., y as f32 / 50., ((val as f32 / 10.0).sin() + 1.) / 2., 1.)))
-                                }
-                            />
-*/
+
 #[widget]
 pub fn top(context: &mut WidgetContext) -> Fragment {
     let frame_counter = context.listenable(0);
@@ -18,10 +11,10 @@ pub fn top(context: &mut WidgetContext) -> Fragment {
 
     rsx! {
         <row main_axis_alignment=MainAxisAlignment::SpaceEvenly>
-            {(0..50).map(|x| rsx!{
-                <column main_axis_alignment=MainAxisAlignment::SpaceEvenly key=&x>
-                    {(0..50).map(|y| rsx! {
-                        <sized_box constraint=BoxConstraints::tight_for(rutter_layout::Size::new(10.0, 10.0)) key=&y>
+            {(0..75).map(|x| rsx!{
+                <column main_axis_alignment=MainAxisAlignment::SpaceEvenly key=x>
+                    {(0..75).map(|y| rsx! {
+                        <sized_box constraint=BoxConstraints::tight_for(rutter_layout::Size::new(10.0, 10.0)) key=y>
                             <fill_rect
                                 color={
                                     let val = context.listen(frame_counter);
@@ -36,7 +29,6 @@ pub fn top(context: &mut WidgetContext) -> Fragment {
     }
 }
 
-
 fn main() {
     let window_builder = WindowBuilder::new()
         .with_title("narui circle benchmark")
@@ -46,6 +38,6 @@ fn main() {
         window_builder,
         rsx_toplevel! {
             <top />
-        },
+        }
     );
 }
